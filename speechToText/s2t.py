63112c5a8d5a7f3d9da4abbe91122d678df4b252
an1604 <nataf12386@gmail.com>
2024-05-03 12:21:43 +0300

    Speech to text recognition and write it into file.

--- speechToText/s2t.py
+++ speechToText/s2t.py
@@ -2,24 +2,26 @@ import random
 import time
 import speech_recognition as sr
 
-stop = False
-msg = ''
-
 
 # this is called from the background thread
 def convert(recognizer, audio):
-    try:
-        print("Google Speech Recognition thinks you said " + recognizer.recognize_google(audio))
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    if filename:  # Checks if the filename var filled already.
+        with open(filename, 'w') as audio_file:
+            try:
+                msg = recognizer.recognize_google(audio)
+                print("Google Speech Recognition thinks you said " + msg)
+                audio_file.write(msg)  # Writing the message to the transcript.
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
 # The objects
+filename = input("Enter file name: ") + '.txt'  # Opening some file to write the transcript
+
 r = sr.Recognizer()
 m = sr.Microphone()
-
 print("Say something!")
 
 with m as source:
@@ -37,3 +39,4 @@ while counter != rnd:  # Doing some stuff while the other thread converting spee
     counter = random.randint(1, 10000)
 
 stop_listening_func(wait_for_stop=False)
+print(f"Conversation transcript saved on: {filename}")
