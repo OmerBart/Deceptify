49117c120df5cb52b803dede7f005bb00caaa8d7
an1604 <nataf12386@cs.colman.ac.il>
2024-07-17 10:44:44 +0300

    refine requirements.txt

--- Server/routes.py
+++ Server/routes.py
@@ -7,15 +7,15 @@ from flask import copy_current_request_context
 from flask import redirect as flask_redirect, jsonify, session, send_file
 from werkzeug.utils import secure_filename
 
-from Server.CallRecorder import CallRecorder
-from Server.Forms.general_forms import *
-from Server.Forms.upload_data_forms import *
+from CallRecorder import CallRecorder
+from Forms.general_forms import *
+from Forms.upload_data_forms import *
 from flask import render_template, url_for, flash, request, send_from_directory
 import Util
 from Util import *
-from Server.data.prompt import Prompt
-from Server.data.Attacks import AttackFactory
-from Server.data.Profile import Profile
+from data.prompt import Prompt
+from data.Attacks import AttackFactory
+from data.Profile import Profile
 from threading import Thread, Event
 from SpeechToText import SpeechToText
 import requests
